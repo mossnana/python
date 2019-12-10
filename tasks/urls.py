b4d8bfb449a9b83a2782b32f237a90be661bb0e1
mossnana <samurai.hanso@gmail.com>
2019-12-11 00:53:27 +0700

    Check and Delete

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.template_render)
+    path('', views.template_render),
+    path('<int:pk>/edit/', views.task_edit),
+    path('<int:pk>/delete/', views.task_delete)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import *
 from .forms import *
@@ -8,7 +8,6 @@ def index(req):
     return HttpResponse('Hello World Response')
 
 def template_render(req):
-    print('>>>>>>>>>>>>> Request', req)
     if req.method == 'POST':
         form = TaskForm(req.POST)
         form.save()
@@ -16,6 +15,17 @@ def template_render(req):
     else:
         context = _task_render(req)
         return render(req, 'tasks/list.html', context)
+
+def task_edit(req, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.complete = not task.complete
+    task.save()
+    return redirect('/tasks', pk=task.pk)
+
+def task_delete(req, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('/tasks', pk=task.pk)
 
 def _task_render(req):
     tasks = Task.objects.all()
